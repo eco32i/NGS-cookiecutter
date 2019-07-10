b8e38dab537606eb34b7ebd801f5449d64f420bd
Ilya Shamovsky <ilya.shamovsky@nyumc.org>
2019-07-10 09:22:04 -0400

    Fixed ref dir location in post_gen_hooks.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ -d {{ cookiecutter.ref_dir }} ]; then
-    ln -s {{ cookiecutter.ref_dir }} ../ref/{{ cookiecutter.ref_name }}
+    ln -s {{ cookiecutter.ref_dir }} ref/{{ cookiecutter.ref_name }}
 else
     exit 1
 fi
